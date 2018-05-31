810a0de3e28e1187c681cf0f21688e0f8063e1e0
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-05-30 20:51:25 -0700

    add divison and multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ var product = function(number1, number2) {
 	return number1*number2;
 };
 
-var quotient = function(number1, number2) {
+var divide = function(number1, number2) {
 	return number1/number2;
 };
 
@@ -27,5 +27,19 @@ $(document).ready(function(){
 		var number2 = parseInt($("input#sum2").val());
 		var result= sum(number1,number2);
 		$("#output").text(result);
+	});
+	$("form#product").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("input#sum3").val());
+		var number2 = parseInt($("input#sum4").val());
+		var result = product(number1,number2);
+		$("#multiply").text(result);
+	});
+	$("form#divide").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("input#sum5").val());
+		var number2 = parseInt($("input#sum6").val());
+		var result = divide(number1,number2);
+		$("div#divide").text(result);
 	});
 });
